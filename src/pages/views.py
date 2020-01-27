9395e6c96815c301764b8dda21a7b7b2064db676
Christopher Nguyen <Chris_ngu@pm.me>
2020-01-26 19:20:43 -0600

    testing context html rendering with template tags

--- src/pages/views.py
+++ src/pages/views.py
@@ -14,7 +14,8 @@ def about_view(request, *args, **kwargs):
     my_context = {
         "my_text" : "this is about us",
         "my_number" : 123,
-        "my_list" : [123, 455, 665]
+        "my_list" : [123, 455, 665, "Abc"],
+        "my_html": "<h1>html tester</h1>"
     }
 
     return render(request, "about.html", my_context)
