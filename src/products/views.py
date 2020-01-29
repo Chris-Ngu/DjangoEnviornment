31ca8d42422ab990b78dfe65b810f807add203a3
Christopher Nguyen <Chris_ngu@pm.me>
2020-01-28 20:50:09 -0600

    testing form w/o django syntax

--- src/products/views.py
+++ src/products/views.py
@@ -3,16 +3,20 @@ from .models import Product
 from .forms import ProductForm
 
 def product_create_view(request):
-  form = ProductForm(request.POST or None)
-  if form.is_valid():
-    form.save()
-    form = ProductForm()
+    context = {}
+    return render(request, "products/product_create.html", context)
 
-  obj = Product.objects.get(id=1)
-  context = {
-    'form': form
-  }
-  return render(request, "products/product_create.html", context)
+#def product_create_view(request):
+ # form = ProductForm(request.POST or None)
+  #if form.is_valid():
+   # form.save()
+    #form = ProductForm()
+
+  #obj = Product.objects.get(id=1)
+  #context = {
+   # 'form': form
+  #}
+  #return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
