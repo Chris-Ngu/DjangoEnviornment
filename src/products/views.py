c99fb8896acb1e731a15d9424e219262d9b68e07
Christopher Nguyen <Chris_ngu@pm.me>
2020-01-31 21:47:57 -0600

    dynamic links and 404 error exception

--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from .forms import ProductForm, RawProductForm
 
@@ -52,3 +53,19 @@ def render_initial_data(request):
     'form': form
   }
   return render(request, "products/product_create.html", context)
+
+def dynamic_lookup_view(request, id):
+  #this is the standard way without 404
+  #obj = Product.objects.get(id=id)
+  #404 error handling here
+  #obj = get_object_or_404(Product,id = id)
+
+  try:
+    obj = Product.objects.get(id=id)
+  except Product.DoesNotExist:
+    raise Http404
+
+  context = {
+    "object" : obj
+  }
+  return render(request, "products/product_detail.html", context)
